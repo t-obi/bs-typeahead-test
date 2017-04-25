462840179fa45c01ef14b9ff141b572cb448bd14
Tobias Kloht <tobias.kloht@gmail.com>
2017-04-25 22:43:21 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Typeahead} from 'react-bootstrap-typeahead';
+import countries from 'country-list';
+
+const contryNames = countries().getNames();
+
+class App extends Component {
+
+  state = {
+    selected: [],
+    controlled: false,
+  };
+
+  handleInputChange = (input) => {
+    console.log('input-change: ', input);
+    this.setState({
+      selected: [input],
+    });
+  };
+
+  handleChange = (selected) => {
+    console.log('onchange: ', selected);
+    this.setState({ selected });
+  };
+
+  render() {
+    const { selected, controlled } = this.state;
+
+    const typeaheadProps = controlled ? {
+      onChange: this.handleChange,
+      onInputChange: this.handleInputChange,
+      selected,
+    } : {};
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="container">
+          <div className="row">
+            <label>
+              controlled
+              <input type="checkbox"
+                checked={this.state.controlled}
+                onChange={() => this.setState({controlled: !controlled})}
+              />
+            </label>
+          </div>
+          <div className="row">
+            <Typeahead
+              options={contryNames}
+              {...typeaheadProps}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
